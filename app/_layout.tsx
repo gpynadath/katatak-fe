28d336b4955890805deb2171f3a232269a76fa0f
hMoffat <hmoffat.work@gmail.com>
2024-02-22 15:00:19 +0000

    Got user context working.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,20 @@
 import { Stack } from "expo-router/stack";
+import CurrentUserContext from "./context/UserContext";
+import { useState, createContext } from "react";
 
 export default function RootLayout() {
+  const [currentUser, setCurrentUser] = useState<UserType>({
+    username: "freezypop",
+    user_id: 1,
+    bio: "I like to sit in the fridge making sick burns about the maternal figure in your life.",
+    avatar_img_url:
+      "https://images.unsplash.com/photo-1497034825429-c343d7c6a68f",
+  });
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <CurrentUserContext.Provider value={currentUser}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </CurrentUserContext.Provider>
   );
 }
