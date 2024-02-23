78b422078103fdfea292016555824c3bac8d707a
Holly Rees <hollyclarerees@gmail.com>
2024-02-23 15:29:32 +0000

    update display for output

--- app/kata-page/Output.tsx
+++ app/kata-page/Output.tsx
@@ -1,67 +1,79 @@
-import { View, Text, Button } from 'react-native';
-import { useState, useEffect } from 'react';
-import { postKata } from '../api';
-import { styles } from './KataPageStyleSheet';
+import { View, Text, Button } from "react-native";
+import { useState, useEffect } from "react";
+import { postKata } from "../api";
+import { styles } from "./KataPageStyleSheet";
 
 interface Output {
-    success: boolean,
-    stderr:	string,
-    stdout:	string,
-    test_results: string,
-    logs: Array<string>,
-    posted_solution: boolean
+  success: boolean;
+  stderr: string;
+  stdout: string;
+  test_results: string;
+  logs: Array<string>;
+  posted_solution: boolean;
 }
 
 interface SendInput {
-    output: Output | undefined, 
-    isLoading: boolean, 
-    error: any
+  output: Output | undefined;
+  isLoading: boolean;
+  error: any;
 }
 
-export default function Output({ kata_id, input } : { kata_id: number, input: string }) {
+export default function Output({
+  kata_id,
+  input,
+}: {
+  kata_id: number;
+  input: string;
+}) {
+  const { output, isLoading, error }: SendInput = useSendInput(kata_id, input);
 
-    const { output, isLoading, error } : SendInput = useSendInput(kata_id, input);
-
-    if (isLoading && input === 'Default') return <Text style={styles.baseText}>Submit your function to pass the tests!</Text>;
-    if (isLoading && input !== 'Default') return <Text>Loading...</Text>;
-    if (error) return <Text>Error...Solution </Text> // Add indepth error handling...
-    if (!output) return <Text>No Output</Text>; 
-
+  if (isLoading && input === "Default")
     return (
-        <>
-        <Text>
-            {output.success 
-            ? 'SUCCESS\n' + output.stderr +  output.stdout 
-            : 'FAIL\n' + output.stderr +  output.stdout }
-        </Text>
-        </>
-    )
-}
+      <Text style={styles.baseText}>
+        Submit your function to pass the tests!
+      </Text>
+    );
+  if (isLoading && input !== "Default") return <Text>Loading...</Text>;
+  if (error) return <Text>Error...Solution </Text>; // Add indepth error handling...
+  if (!output) return <Text>No Output</Text>;
 
+  return (
+    <>
+      <Text>
+        {output.success
+          ? "SUCCESS\n" +
+            output.test_results +
+            "\nLogs (remember, any console.logs will log for each test..):\n" +
+            output.logs
+          : "FAIL\n" + output.test_results + "\nLogs:\n" + output.logs}
+      </Text>
+    </>
+  );
+}
 
 function useSendInput(kata_id: number, input: string) {
-    const [output, setOutput] = useState<Output>();
-    const [isLoading, setLoading] = useState(true); 
-    const [error, setError] = useState(null); 
+  const [output, setOutput] = useState<Output>();
+  const [isLoading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-    const sendInput = async () => {
-        setLoading(true);
-        try {
-            const response : Output = await postKata(kata_id, input);
-            setOutput(response);
-            setError(null);
-        } catch (err: any) {
-            setError(err);
-        } finally { 
-            setLoading(false);
-        }
-    };
+  const sendInput = async () => {
+    setLoading(true);
+    try {
+      const response: Output = await postKata(kata_id, input);
+      setOutput(response);
+      setError(null);
+    } catch (err: any) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    useEffect(() => {
-        if(input !== 'Default') {
-            sendInput();
-        }
-    }, [input]);
+  useEffect(() => {
+    if (input !== "Default") {
+      sendInput();
+    }
+  }, [input]);
 
-    return { output, isLoading, error };
+  return { output, isLoading, error };
 }
