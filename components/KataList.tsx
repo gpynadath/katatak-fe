452520b59a7cf03880cb28bc646985f8215a11f7
Holly Rees <hollyclarerees@gmail.com>
2024-02-27 15:38:45 +0000

    add SOLVED and topics to kata cards

--- components/KataList.tsx
+++ components/KataList.tsx
@@ -1,8 +1,10 @@
 import { ScrollView, View, Text, StyleSheet } from "react-native";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import KataCard from "./KataCard";
+import CurrentUserContext from "app/context/UserContext";
 
-import { getAllKatas } from "app/api";
+import { getAllKatas, getSolutionsByUserId } from "app/api";
+import currentKata from "app/(tabs)/CurrentKata";
 
 type KataListProps = {
   topicsValue: string;
@@ -13,16 +15,36 @@ type kataObj = {
   kata_name: string;
   description: string;
   difficulty: string;
+  topics: string[];
+};
+type solutionObj = {
+  kata_id: number;
+  kata_name: string;
+  solution: string;
+  solution_id: number;
+  user_id: number;
+  votes: number;
 };
 
 export default function KataList({ topicsValue, orderValue }: KataListProps) {
   const [kataData, setKataData] = useState<kataObj[]>([]);
   const [isLoading, setLoading] = useState(true);
+  const currentUser = useContext(CurrentUserContext);
+  const [usersSolvedKatas, setUsersSolvedKatas] = useState<number[]>([]);
 
   useEffect(() => {
     setLoading(true);
 
     const fetchData = async () => {
+      const allSolutions = await getSolutionsByUserId(currentUser.user_id);
+
+      const solvedKatasIds: number[] = [];
+      allSolutions.map((solution: solutionObj) => {
+        if (!solvedKatasIds.includes(solution.kata_id))
+          solvedKatasIds.push(solution.kata_id);
+      });
+
+      setUsersSolvedKatas(solvedKatasIds);
       if (topicsValue === "All Topics...") {
         const data = await getAllKatas("", orderValue);
         setKataData(data.data.katas);
@@ -39,17 +61,21 @@ export default function KataList({ topicsValue, orderValue }: KataListProps) {
   if (isLoading) return <Text>Loading...</Text>;
   return (
     <ScrollView>
-    <View style={styles.container}>
-      {kataData.map((kata) => {
-        return <KataCard kata={kata} key={kata.kata_id} />
-      })}
-    </View>
+      <View style={styles.container}>
+        {kataData.map((kata) => {
+          let solved = false;
+          if (usersSolvedKatas.includes(kata.kata_id)) {
+            solved = true;
+          }
+          return <KataCard kata={kata} key={kata.kata_id} solved={solved} />;
+        })}
+      </View>
     </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#F2F2D0'
+    backgroundColor: "#F2F2D0",
   },
 });
