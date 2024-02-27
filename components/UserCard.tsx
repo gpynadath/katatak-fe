1d05fa3daae61518275ba8c7fd2855ab17208530
Holly Rees <hollyclarerees@gmail.com>
2024-02-27 17:15:40 +0000

    add styling to user page

--- components/UserCard.tsx
+++ components/UserCard.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { useContext, useEffect, useState } from "react";
 import CurrentUserContext from "app/context/UserContext";
 import { getSolutionsByUserId, getKata } from "app/api";
+import { useFonts } from "expo-font";
 
 type solutionObj = {
   kata_id: number;
@@ -61,22 +62,41 @@ export default function UserCard() {
     fetchData();
   }, []);
 
+  const [fontsLoaded, fontError] = useFonts({
+    Pixellari: require("../assets/fonts/Pixellari.ttf"),
+    dogica: require("../assets/fonts/dogica.ttf"),
+  });
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   if (isLoading) return <Text>Loading...</Text>;
   return (
-    <View style={styles.container}>
-      <Image
-        source={{
-          uri: currentUser.avatar_img_url,
-        }}
-        alt={`${currentUser.username}'s avatar`}
-        style={styles.img}
-      />
-      <Text style={[styles.usernameHeader]}>{currentUser.username}</Text>
-      <Text style={styles.bio}>"{currentUser.bio}"</Text>
-      <Text>Number Of Katas Completed: {numKatasCompleted}</Text>
-      <Text>Katas Completed: {kataNamesCompleted.join(", ")}</Text>
-      <Text>Topics Done: {topicsDone.join(", ")}</Text>
-    </View>
+    <>
+      <View style={styles.container}>
+        <Image
+          source={{
+            uri: currentUser.avatar_img_url,
+          }}
+          alt={`${currentUser.username}'s avatar`}
+          style={styles.img}
+        />
+        <Text style={styles.usernameHeader}>{currentUser.username}</Text>
+        <Text style={styles.bio}>"{currentUser.bio}"</Text>
+        <View style={styles.statsContainer}>
+          <Text style={styles.topicText}>
+            Number Of Katas Completed: {numKatasCompleted}
+          </Text>
+          <Text style={styles.topicText}>
+            Katas Completed: {"\n\n > " + kataNamesCompleted.join("\n > ")}
+          </Text>
+          <Text style={styles.topicText}>
+            Topics Done: {"\n\n > " + topicsDone.join("\n > ")}
+          </Text>
+        </View>
+      </View>
+    </>
   );
 }
 
@@ -86,10 +106,18 @@ const styles = StyleSheet.create({
     display: "flex",
     alignItems: "center",
     flex: 3,
+    backgroundColor: "#F2F2D0",
+  },
+  statsContainer: {
+    backgroundColor: "#F2F2D0",
+    textAlign: "left",
   },
   usernameHeader: {
-    fontWeight: "bold",
-    fontSize: 30,
+    fontFamily: "Pixellari",
+    fontSize: 25,
+    marginLeft: 17,
+    marginRight: 17,
+    marginTop: 10,
   },
   img: {
     padding: 50,
@@ -97,10 +125,25 @@ const styles = StyleSheet.create({
     width: 200,
     borderRadius: 100,
     marginTop: marginTop,
+    marginBottom: 15,
   },
   bio: {
     margin: 20,
-    fontFamily: "Arial",
-    fontSize: 20,
+    fontFamily: "Pixellari",
+    fontSize: 15,
+    marginBottom: 30,
+  },
+  topicText: {
+    fontFamily: "dogica",
+    fontSize: 9,
+    marginBottom: 15,
+
+    padding: 5,
+  },
+  content: {
+    marginLeft: 10,
+  },
+  footer: {
+    alignItems: "center",
   },
 });
