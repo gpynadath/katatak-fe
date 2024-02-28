a9771a0bc7c7ece05ebd20002f17d429fa3f41dc
Holly Rees <hollyclarerees@gmail.com>
2024-02-28 15:04:59 +0000

    add styling to tabs and upper screen

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,4 +1,5 @@
 import { AntDesign } from "@expo/vector-icons";
+import { StyleSheet } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
 import { useContext } from "react";
@@ -7,13 +8,17 @@ import CurrentUserContext from "app/context/UserContext";
 export default function TabsLayout() {
   const currentUser = useContext(CurrentUserContext);
   return (
-    <Tabs>
+    <Tabs sceneContainerStyle={{ backgroundColor: "#F2F2D0" }}>
       <Tabs.Screen
         name="index"
         options={{
           title: "Home",
           headerShown: false,
           tabBarIcon: () => <AntDesign name="home" size={24} color="black" />,
+          tabBarInactiveBackgroundColor: "#D8AC5B",
+          tabBarActiveBackgroundColor: "#FFCD6B",
+          tabBarActiveTintColor: "black",
+          tabBarInactiveTintColor: "black",
         }}
       />
       <Tabs.Screen
@@ -24,6 +29,10 @@ export default function TabsLayout() {
           tabBarIcon: () => (
             <AntDesign name="codesquareo" size={24} color="black" />
           ),
+          tabBarInactiveBackgroundColor: "#D8AC5B",
+          tabBarActiveBackgroundColor: "#FFCD6B",
+          tabBarActiveTintColor: "black",
+          tabBarInactiveTintColor: "black",
         }}
       />
       <Tabs.Screen
@@ -32,8 +41,18 @@ export default function TabsLayout() {
           title: currentUser.username,
           headerShown: false,
           tabBarIcon: () => <AntDesign name="user" size={24} color="black" />,
+          tabBarInactiveBackgroundColor: "#D8AC5B",
+          tabBarActiveBackgroundColor: "#FFCD6B",
+          tabBarActiveTintColor: "black",
+          tabBarInactiveTintColor: "black",
         }}
       />
     </Tabs>
   );
 }
+
+const styles = StyleSheet.create({
+  bar: {
+    backgroundColor: "#FFCD6B",
+  },
+});
